2f422b746645cd10b8f3de796616ad93acfbbe18
akbarfarajov <akbar.farajov@fintlabs.com>
2025-07-23 22:09:32 +0400

    Refactor TabIcon component and update tab names; add Saved screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,32 @@
+import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text, View } from "react-native";
+
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Tabs } from "expo-router";
-import React, { FC } from "react";
-import {
-  Image,
-  ImageBackground,
-  ImageSourcePropType,
-  Text,
-  View,
-} from "react-native";
 
-type TabIconProps = {
-  focused: boolean;
-  icon: ImageSourcePropType;
-  title: string;
-};
-
-const TabIcon: FC<TabIconProps> = ({ focused, icon, title }) => {
-  if (!focused) {
+function TabIcon({ focused, icon, title }: any) {
+  if (focused) {
     return (
-      <View className="min-h-16 flex items-center justify-center mt-4 rounded-full">
-        <Image source={icon} tintColor="#a8b5db" className="size-5" />
-      </View>
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
     );
   }
-  return (
-    <ImageBackground
-      source={images.highlight}
-      className="flex flex-row justify-center items-center min-w-28 flex-1 min-h-16 mt-4 rounded-full overflow-hidden"
-    >
-      <Image source={icon} tintColor="#151312" className="size-5" />
-      <Text className="text-base font-semibold text-secondary ml-2">
-        {title}
-      </Text>
-    </ImageBackground>
-  );
-};
 
-const _Layout = () => {
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+}
+
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
@@ -45,32 +34,33 @@ const _Layout = () => {
         tabBarItemStyle: {
           width: "100%",
           height: "100%",
-          alignItems: "center",
           justifyContent: "center",
+          alignItems: "center",
         },
         tabBarStyle: {
           backgroundColor: "#0F0D23",
           borderRadius: 50,
           marginHorizontal: 20,
           marginBottom: 36,
+          height: 52,
           position: "absolute",
           overflow: "hidden",
-          height: 52,
+          borderWidth: 0,
+          borderColor: "#0F0D23",
         },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "index",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} icon={icons.home} title="Home" />
-            </>
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
           ),
         }}
       />
+
       <Tabs.Screen
         name="search"
         options={{
@@ -81,6 +71,18 @@ const _Layout = () => {
           ),
         }}
       />
+
+      <Tabs.Screen
+        name="save"
+        options={{
+          title: "Save",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Save" />
+          ),
+        }}
+      />
+
       <Tabs.Screen
         name="profile"
         options={{
@@ -91,18 +93,6 @@ const _Layout = () => {
           ),
         }}
       />
-      <Tabs.Screen
-        name="saved"
-        options={{
-          title: "Saved",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.save} title="Saved" />
-          ),
-        }}
-      />
     </Tabs>
   );
-};
-
-export default _Layout;
+}
